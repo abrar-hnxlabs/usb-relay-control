74248b3c0372cad1e014e35d4e0b84497250e977
Abrar <abrar@hnxlabs.com>
2022-04-19 03:49:25 +0000

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+import { SimpleGrid, Box, Heading } from "@chakra-ui/core";
+import { RelayControl } from "./components/RelayControl";
+import { IListResponse, IRequest, IResponse } from "../../types/WebSocketTypes";
+
+function App() {
+  const [deviceList, setDeviceList] = useState<IResponse[]>([]);
+  const [ws, setWs] = useState<WebSocket>(new WebSocket("ws://192.168.1.65:8080/relay"));
+
+  useEffect(() => {
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        operation: "list"
+      }));
+    };
+
+    ws.onmessage = (e) => {
+      const data: IListResponse = JSON.parse(e.data);
+      if (data.operation === "list" ){
+        const d = JSON.parse(e.data);
+        setDeviceList(d.data);
+      } else if (data.operation === "change"){
+        const d = JSON.parse(e.data);
+        const newDeviceList = deviceList.map(dev => {
+          if(dev.deviceId === d.data[0].deviceId){
+            return d.data[0];
+          }
+          return dev;
+        });
+        setDeviceList(newDeviceList);
+      }
+    };
+
+    ws.onclose= () => {
+      setWs(new WebSocket("ws://192.168.1.65:8080/relay"));
+    }
+  }, [ws, deviceList]);
+
+  const relayOnChange = (deviceId: number, value: string, revertDelay: number) => {
+    const req: IRequest = {
+      deviceId,
+      operation: value,
+      revertDelay
+    }
+    ws.send(JSON.stringify(req));
+  }
+
+  return (
+    <SimpleGrid columns={1} spacing={3} >
+      <Box bg="tomato">
+        <Heading ml={5} color="white" >Automation Relay State</Heading>
+      </Box>
+      { deviceList.map( (d: any) => <RelayControl
+        deviceId={d.deviceId}
+        name={d.deviceName}
+        key={d.deviceId}
+        checked={(d.state === "on"? true: false )}
+        onChange={relayOnChange}
+      />)}
+    </SimpleGrid>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/RelayControl/index.tsx
@@ -0,0 +1,49 @@
+import React, { MouseEventHandler, useEffect, useState } from 'react';
+
+import { Text, Box, Switch, Stack, CircularProgress, Divider, IconButton } from "@chakra-ui/core";
+import { RepeatClockIcon } from "@chakra-ui/icons";
+
+interface RelayControlProps {
+    name: string;
+    deviceId: number;
+    checked: boolean;
+    onChange: Function;
+}
+
+export const RelayControl = (props: RelayControlProps) => {
+  const { name, deviceId, checked, onChange} = props;
+  const [saving, setSaving] = useState(false);
+
+  const switchOnClick = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    onChange(deviceId, (checked)? "on": "off", -1);
+    setSaving(true);
+  }
+
+  const momentaryBtnClick = (e: any) => {
+    onChange(deviceId, "on", 500);
+    setSaving(true);
+  }
+
+  const delayBtnClick = (e: any) => {
+    onChange(deviceId, "on", 10000);
+    setSaving(true);
+  }
+
+  useEffect(()=> {
+    setSaving(false);
+  },[checked]);
+
+  return (
+      <Box>
+        <Stack isInline align="center" direction="row" >
+          <Text fontSize="xl" ml={5} width="40%" >{name}</Text>
+          <Switch size="lg" onChange={switchOnClick} isChecked={checked} />
+          <IconButton aria-label="momentary" icon="repeat-clock" variantColor="blue" variant="outline" onClick={momentaryBtnClick}/>
+          <IconButton aria-label="momentary" icon="time" variantColor="red" variant="outline" onClick={delayBtnClick}/>
+          {saving && <CircularProgress isIndeterminate  thickness={0.3} color="orange" /> }
+        </Stack>
+        <Divider />
+      </Box>
+  );
+}
